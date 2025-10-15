addecfcc2f968abb0f67a82c1e38422677d0c0a7
Santiago Cienfuegos G. <santiago.cienfuegos@uc.cl>
2025-10-15 19:50:08 -0300

    PC act 15-10

--- project_1.srcs/sources_1/new/clock_divider.v
+++ project_1.srcs/sources_1/new/clock_divider.v
@@ -1,7 +1,14 @@
 `timescale 1ns / 1ps
 
 module clock_divider(
-    input clk,
-    output clk_out
+    input logic [16:0]clk,
+    input logic rst,
+    output logic [16:0]clk_out
     );
+
+assign DIV = 16'b0; 
+
+always_ff@(posedge clk) //rst síncrono, consideramos clock de 16 bits, que es poco mas de 60 segundos
+    if (rst) clk_out <= 16'b0;
+    else clk_out <= clk_out + 1;
 endmodule
--- project_1.srcs/sources_1/new/seq4.v
+++ project_1.srcs/sources_1/new/seq4.v
@@ -1,9 +1,29 @@
 `timescale 1ns / 1ps
 
-module seq4(
-    input clk,
-    input rst,
-    input en,
-    output [3:0] q
-    );
+module seq4(input logic clk,
+            input logic rst,
+            input logic en,
+            output logic[3:0]Q  );
+
+    logic [3:0]D ;
+    
+    always_comb begin
+        case (Q)
+            4'b0000: D = 4'b1000;
+            4'b1000: D = 4'b1100;
+            4'b1100: D = 4'b1110;
+            4'b1110: D = 4'b1111;
+            4'b1111: D = 4'b0111;
+            4'b0111: D = 4'b0011;
+            4'b0011: D = 4'b0001;
+            4'b0001: D = 4'b0000;
+            default: D = 4'b0000; // atrapa errores, dejamos cómo default el inicial
+        endcase
+    end
+    
+    always_ff@(posedge clk)//rst y en síncrono
+        if (rst) Q <= 4'b0; //con rst = 1 -> registra 0000
+        else
+            if (en) Q <= D; //con en = 1  -> avanza al siguiente estado
+
 endmodule
