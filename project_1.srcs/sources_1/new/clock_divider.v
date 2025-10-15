12030669fdb5fe98e6dc4c39b70abf96a0fbd9a9
Santiago Cienfuegos G. <santiago.cienfuegos@uc.cl>
2025-10-15 15:28:51 -0300

    Initial commit

---
+++ project_1.srcs/sources_1/new/clock_divider.v
@@ -0,0 +1,7 @@
+`timescale 1ns / 1ps
+
+module clock_divider(
+    input clk,
+    output clk_out
+    );
+endmodule
---
+++ project_1.srcs/sources_1/new/seq4.v
@@ -0,0 +1,9 @@
+`timescale 1ns / 1ps
+
+module seq4(
+    input clk,
+    input rst,
+    input en,
+    output [3:0] q
+    );
+endmodule
